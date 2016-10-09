8f06f6abfc1064b6214d3f069285c95ea07b7b00
Ranmocy Sheng <ranmocy@gmail.com>
2016-10-09 09:30:08 -0700

    add hyper key

--- src/share/types.hpp
+++ src/share/types.hpp
@@ -114,6 +114,7 @@ enum class key_code : uint32_t {
   // predefined virtual modifier flags
   vk_none,
   vk_fn_modifier,
+  vk_hyper_modifier,
 
   // virtual key codes
   vk_consumer_brightness_down,
@@ -191,6 +192,7 @@ enum class modifier_flag : uint32_t {
   right_option,
   right_command,
   fn,
+  hyper,
   prepared_modifier_flag_end_
 };
 
@@ -221,6 +223,8 @@ public:
       return modifier_flag::right_command;
     case static_cast<uint32_t>(key_code::vk_fn_modifier):
       return modifier_flag::fn;
+    case static_cast<uint32_t>(key_code::vk_hyper_modifier):
+      return modifier_flag::hyper;
     default:
       return modifier_flag::zero;
     }
@@ -416,6 +420,7 @@ public:
 
           // Extra
           {"fn", key_code::vk_fn_modifier},
+          {"hyper", key_code::vk_hyper_modifier}, // equals to ctrl + option + cmd
           {"vk_consumer_brightness_down", key_code::vk_consumer_brightness_down},
           {"vk_consumer_brightness_up", key_code::vk_consumer_brightness_up},
           {"vk_consumer_illumination_down", key_code::vk_consumer_illumination_down},
@@ -639,6 +644,7 @@ public:
       map[key_code(kHIDUsage_KeyboardRightGUI)] = 0x36;
 
       map[key_code::vk_fn_modifier] = 0x3f;
+      map[key_code::vk_hyper_modifier] = 0x84;
       map[key_code::vk_dashboard] = 0x82;
       map[key_code::vk_launchpad] = 0x83;
       map[key_code::vk_mission_control] = 0xa0;
